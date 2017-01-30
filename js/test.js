f7d55f4a9656a4abbc1eb432e37c6e944d04c4e4
Anupriya Ankolekar <anupriya@anupriyas-MacBook-Pro.local>
2017-01-30 12:01:19 -0800

    more

--- js/test.js
+++ js/test.js
@@ -1,10 +1,24 @@
 !function() {
   var svgContainer = d3.select("#vizContainer").append("svg")
-                                     .attr("width", 200)
-                                     .attr("height", 200);
+                      .attr("width", 800)
+                      .attr("height", 450);
  
-  var circle = svgContainer.append("circle")
-                          .attr("cx", 30)
-                          .attr("cy", 30)
-                          .attr("r", 20);
+  var svg = d3.select("svg");
+
+  //var circle = svgContainer.append("circle")
+  //                .attr("cx", 30)
+  //                .attr("cy", 30)
+  //                .attr("r", 20)
+  //                .style("fill", "steelblue");
+
+  var circle = svg.selectAll("circle")
+    .data([32, 57, 293], function(d) { return d; });
+
+  circle.enter().append("circle")
+    .attr("cy", 60)
+    .attr("cx", function(d, i) { return i * 100 + 30; })
+    .attr("r", function(d) { return Math.sqrt(d); })
+    .style("fill", "steelblue");
+
+  circle.exit().remove();
 }();
